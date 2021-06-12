e4be821dadc3577cf6f6eb94b5cd6aa30c0d5203
Greta Skipare <gretchfurlined@gmail.com>
2021-06-12 09:18:05 +0300

    .

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,25 @@ Visas funkcijas iškvieskite vieną po kitos, žemiau šio komentaro.
 Taip pat parašykite funkciją kuri: isikviesdama save pačią atspausdina skaičius nuo 1 iki 10
 
 */
+
+
+countCaffe();
+countCaffeKaunas();
+countMeals();
+
+let dataArray = 
+
+function countCaffe(params) {
+    
+};
+
+function countCaffeKaunas(params) {
+    
+};
+
+function countMeals(params) {
+    
+};
+
+
+
